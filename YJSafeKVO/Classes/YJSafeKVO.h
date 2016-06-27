15208fd286636087fb86c525ee95ea80768eda56
huang-kun <jack-huang-developer@foxmail.com>
2016-06-27 23:17:02 +0800

    Add missing header file.

---
+++ YJSafeKVO/Classes/YJSafeKVO.h
@@ -0,0 +1,11 @@
+//
+//  YJSafeKVO.h
+//  YJKit
+//
+//  Created by huang-kun on 16/6/27.
+//  Copyright © 2016年 huang-kun. All rights reserved.
+//
+
+#import <YJSafeKVO/NSObject+YJRuntimeEncapsulation.h>
+#import <YJSafeKVO/NSObject+YJSafeKVO.h>
+#import <YJSafeKVO/YJKVCMacros.h>
